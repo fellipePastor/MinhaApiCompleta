f6845eb9cce98bf891425aeef3d5753d9e2ab37a
Fellipe Henrique Pastor Pereira <fellipe.pereira@mrv.com.br>
2022-04-24 21:16:33 -0300

    adicionando confiigs de identity

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -1,13 +1,70 @@
-﻿using DevIO.Business.Intefaces;
+﻿using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace DevIO.Api.Controllers
 {
-    [Route("api/conta")]
+    [Route("api")]
     public class AuthController : MainController
     {
-        public AuthController(INotificador notificador) : base(notificador)
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotificador notificador,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notificador)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        [HttpPost("new-account")]
+        public async Task<ActionResult> Register(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+            foreach (var erro in result.Errors)
+            {
+                NotificarErro(erro.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUserViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUserViewModel.Email, loginUserViewModel.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse();
+            }
+            if(result.IsLockedOut)
+            {
+                NotificarErro("Usuário temporariamente bloqueado por tentativas inválidas");
+                return CustomResponse(loginUserViewModel);
+            }
+            NotificarErro("Usuário ou Senha incorretos");
+
+            return CustomResponse(loginUserViewModel);
         }
     }
 }
+ 
